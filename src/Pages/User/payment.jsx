123fe5f0b9088fe68663b498766a3f3f78de3e9a
lina-22 <haquefahinur@gmail.com>
2023-06-10 15:13:01 +0200

    some refonte updated

--- src/Pages/User/payment.jsx
+++ src/Pages/User/payment.jsx
@@ -1,18 +1,18 @@
 import "../../CSS_User/payment.css";
 
-import paymentImg from "../../Images/paymentImage.PNG";
+// import paymentImg from "../../Images/payment_method/paymentImage.PNG";
 import { Button, Container, Table } from "react-bootstrap";
 
 function Payment() {
   return (
     <Container className="mx-auto">
-    <div className="payment">
-      <section className="paymentImg">
-        <img src={paymentImg} alt="" />
-      </section>
-    </div>
+      <div className="payment">
+        <section className="paymentImg">
+          {/* <img src={paymentImg} alt="" /> */}
+        </section>
+      </div>
     </Container>
   );
 }
 
-export default Payment; 
+export default Payment;
